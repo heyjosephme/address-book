76f4d5cccc727a271207f6db6337d7b97812da13
Joseph <166889171+0xj0s3ph@users.noreply.github.com>
2025-03-17 12:46:39 +0900

    feat: synchroize URLs to Form state

--- app/layouts/sidebar.tsx
+++ app/layouts/sidebar.tsx
@@ -1,17 +1,25 @@
 import { Form, Link, NavLink, Outlet, useNavigation } from "react-router";
 import { getContacts } from "../data";
 import type { Route } from "./+types/sidebar";
+import { useEffect } from "react";
 
 export async function loader({ request }: Route.LoaderArgs) {
   const url = new URL(request.url);
   const q = url.searchParams.get("q");
   const contacts = await getContacts(q);
-  return { contacts };
+  return { contacts, q };
 }
 
 export default function SidebarLayout({ loaderData }: Route.ComponentProps) {
-  const { contacts } = loaderData;
+  const { contacts, q } = loaderData;
   const navigation = useNavigation();
+  useEffect(() => {
+    const searchField = document.getElementById("q");
+    if (searchField instanceof HTMLInputElement) {
+      searchField.value = q || "";
+    }
+  }, [q]);
+
   return (
     <>
       <div id="sidebar">
@@ -26,6 +34,7 @@ export default function SidebarLayout({ loaderData }: Route.ComponentProps) {
               name="q"
               placeholder="Search"
               type="search"
+              value={q || ""}
             />
             <div aria-hidden hidden={true} id="search-spinner" />
           </Form>
